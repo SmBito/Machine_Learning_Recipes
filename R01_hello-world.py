972a91854d08747ec716ec9384b39c40cd7eb3a9
sameh-bilto <34525406+sameh-bilto@users.noreply.github.com>
2017-12-14 00:51:55 +0100

    Create R01_hello-world.py

---
+++ R01_hello-world.py
@@ -0,0 +1,15 @@
+from sklearn import tree
+
+#Training Data
+#smooth=1 / bumpy=0
+features = [[140,1],[130,1],[150,0],[170,0]]
+# apple=0 / orange=1
+labels = [0,0,1,1]
+
+#Training Classifier
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+
+#Make Predictions
+print(clf.predict([[162, 0]]))
+print(clf.predict([[102, 1]]))
